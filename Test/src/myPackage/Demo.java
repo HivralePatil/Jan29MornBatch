2bc88bfbb6d9329c523611ae500473074e129a44
HivralePatil <106471676+HivralePatil@users.noreply.github.com>
2022-05-29 16:37:29 +0530

    Update Demo.java

--- Test/src/myPackage/Demo.java
+++ Test/src/myPackage/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
 	public static void main (String[] args) {
 		
 		System.out.println("hello world");
+		System.out.println("hello world 2");
+
+		System.out.println("hello 1");
+        System.out.println("hello world 4");
+
 	}
 
 }
